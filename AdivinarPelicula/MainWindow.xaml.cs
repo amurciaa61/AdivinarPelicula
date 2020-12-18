da8a7b92c1961ff4bc2ac81363e29f8438ec3c2e
Antonio Murcia <amurciaa61@gmail.com>
2020-12-18 08:33:16 +0100

    Hecho el diseño de la pestaña Gestionar e implementados todos los botones, faltará revisión, inccluir plantillas de algún control y mejorar diseño

--- AdivinarPelicula/MainWindow.xaml.cs
+++ AdivinarPelicula/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +11,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace AdivinarPelicula
 {
@@ -20,9 +26,75 @@ namespace AdivinarPelicula
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Pelicula> peliculas;
         public MainWindow()
         {
             InitializeComponent();
+            peliculas = Pelicula.GetSamples();
+            ObservableCollection<string> tipoGenero = new ObservableCollection<string> { "Acción", "Drama", "Comedia", "Terror", "Ciencia-ficción" };
+            generoComboBox.ItemsSource = tipoGenero;
+            listaListBox.DataContext = peliculas;
+        }
+
+        private void listaListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gestionarGrid.DataContext = (sender as ListBox).SelectedItem;
+        }
+
+        private void importarJsonButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult respuesta = MessageBox.Show("Se van a borrar las películas de la lista, ¿conforme?", "Conformidad",
+                                                         MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (respuesta == MessageBoxResult.OK)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                List<Pelicula> pl;
+                openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                openFileDialog.Filter = "Json files |*.json";
+                try
+                {
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        using (StreamReader jsonStream = File.OpenText(openFileDialog.FileName))
+                        {
+                            peliculas.Clear();  // Limpiamos la lista de películas
+                            var json = jsonStream.ReadToEnd();
+                            pl = JsonConvert.DeserializeObject<List<Pelicula>>(json);
+                            foreach (Pelicula peli in pl)
+                            {
+                                peliculas.Add(peli);
+                            }
+                        }
+                        MessageBox.Show("Se ha importado el fichero JSON '" + openFileDialog.FileName + "'",
+                                        "Importacion Json", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Errores", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private void exportarButton_Click(object sender, RoutedEventArgs e)
+        {
+            string peliculasJson = JsonConvert.SerializeObject(peliculas);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Json files |*.json";
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.DefaultExt = "json";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, peliculasJson);
+                    string texto = "Fichero Json: " + saveFileDialog.FileName + " generado en " + Directory.GetCurrentDirectory();
+                    MessageBox.Show(texto, "Exportación Json", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Errores", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
---
+++ AdivinarPelicula/Pelicula.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivinarPelicula
+{
+    class Pelicula : INotifyPropertyChanged
+    {
+
+        private string _titulo;
+
+        public string Titulo
+        {
+            get { return _titulo; }
+            set
+            {
+                if (_titulo != value)
+                {
+                    _titulo = value;
+                    NotifyPropertyChanged("Titulo");
+                }
+            }
+        }
+        private string _pista;
+
+        public string Pista
+        {
+            get { return _pista; }
+            set
+            {
+                if (_pista != value)
+                {
+                    _pista = value;
+                    NotifyPropertyChanged("Pista");
+                }
+            }
+        }
+        private string _imagen;
+
+        public string Imagen
+        {
+            get { return _imagen; }
+            set
+            {
+                if (_imagen != value)
+                {
+                    _imagen = value;
+                    NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+        private string _genero;
+
+        public string Genero
+        {
+            get { return _genero; }
+            set
+            {
+                if (_genero != value)
+                {
+                    _genero = value;
+                    NotifyPropertyChanged("Genero");
+                }
+            }
+        }
+        private bool _facil;
+
+        public bool Facil
+        {
+            get { return _facil; }
+            set
+            {
+                if (_facil != value)
+                {
+                    _facil = value;
+                    NotifyPropertyChanged("Facil");
+                }
+            }
+        }
+        private bool _normal;
+        public bool Normal
+        {
+            get { return _normal; }
+            set
+            {
+                if (_normal != value)
+                {
+                    _normal = value;
+                    NotifyPropertyChanged("Normal");
+                }
+            }
+        }
+        private bool _dificil;
+        public bool Dificil
+        {
+            get { return _dificil; }
+            set
+            {
+                if (_dificil != value)
+                {
+                    _dificil = value;
+                    NotifyPropertyChanged("Dificil");
+                }
+            }
+        }
+
+        public Pelicula()
+        {
+        }
+
+        public Pelicula(string titulo, string pista, string imagen, string genero, bool facil, bool normal, bool dificil)
+        {
+            Titulo = titulo;
+            Pista = pista;
+            Imagen = imagen;
+            Genero = genero;
+            Facil = facil;
+            Normal = normal;
+            Dificil = dificil;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static ObservableCollection<Pelicula> GetSamples()
+        {
+            ObservableCollection<Pelicula> peliculas = new ObservableCollection<Pelicula>();
+            
+            peliculas.Add(new Pelicula("Mision imposible", "Tom Cruise en difícil acción ... ",
+                "E:/DAM2A/Desarrollo Interfaces/Proyectos/AdivinarPelicula/AdivinarPelicula/Imagenes/misionImposible.jpg",
+                "Acción", false, true, false));
+           
+            peliculas.Add(new Pelicula("Torrente, el brazo tonto de la ley", "Santiago Segura - To1",
+                "E:/DAM2A/Desarrollo Interfaces/Proyectos/AdivinarPelicula/AdivinarPelicula/Imagenes/torrente1.jpg",
+                "Comedia", false, false, true));
+            peliculas.Add(new Pelicula("Ben-Hur", "Un clásico de Semana Santa",
+                "E:/DAM2A/Desarrollo Interfaces/Proyectos/AdivinarPelicula/AdivinarPelicula/Imagenes/benhur.jpg",
+                "Drama", false, false, true));
+            peliculas.Add(new Pelicula("El rey león", "Hakuna Matata",
+                "E:/DAM2A/Desarrollo Interfaces/Proyectos/AdivinarPelicula/AdivinarPelicula/Imagenes/elreyleon.jpg",
+                "Ciencia-ficción", false, true, false));
+            
+            return peliculas;
+        }
+    }
+}
